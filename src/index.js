2499cfa1f5af1be2cf68a250a45012c50e453643
Linh_ASUS <vanlinh042004@gmail.com>
2024-11-16 11:57:40 +0700

    Banner Khuyen mai, Sp ban chay, Feedback

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const path = require('path');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const path = require("path");
+const { PrismaClient } = require("@prisma/client");
 const app = express();
 const port = 3000;
-const { engine } = require('express-handlebars');
-const { route } = require('./routes/index');
-const cookieParser = require('cookie-parser');
+const { engine } = require("express-handlebars");
+const { route } = require("./routes/index");
+const cookieParser = require("cookie-parser");
 app.use(express.json());
 // const prisma = new PrismaClient();
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 // Example route using Prisma
-app.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 route(app);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- src/public/apps/script.js
+++ src/public/apps/script.js
@@ -1,32 +1,32 @@
 function showNav() {
-    const navBar = document.querySelector(".header__sidebar");
-    navBar.style.transform = "translateX(0)";
-    navBar.style.opacity = "1";
-    const navBtn = document.querySelector(".header__btn__nav");
-    navBtn.style.opacity = "0.3";
+  const navBar = document.querySelector(".header__sidebar");
+  navBar.style.transform = "translateX(0)";
+  navBar.style.opacity = "1";
+  const navBtn = document.querySelector(".header__btn__nav");
+  navBtn.style.opacity = "0.3";
 }
 
 function hideNav() {
-    const navBar = document.querySelector(".header__sidebar");
-    navBar.style.transform = "translateX(100%)";
-    navBar.style.opacity = "0";
-    const navBtn = document.querySelector(".header__btn__nav");
-    navBtn.style.opacity = "1";
+  const navBar = document.querySelector(".header__sidebar");
+  navBar.style.transform = "translateX(100%)";
+  navBar.style.opacity = "0";
+  const navBtn = document.querySelector(".header__btn__nav");
+  navBtn.style.opacity = "1";
 }
 
 function notify(obj) {
-    const main = document.getElementById('notify');
-    const icons = {
-        success: 'fa-check',
-        error: 'fa-info',
-        warning: 'fa-exclamation',
-    }
-    if (main) {
-        const el = document.createElement('div');
-        el.classList.add('notify');
-        el.classList.add('notify--' + obj.type);
+  const main = document.getElementById("notify");
+  const icons = {
+    success: "fa-check",
+    error: "fa-info",
+    warning: "fa-exclamation",
+  };
+  if (main) {
+    const el = document.createElement("div");
+    el.classList.add("notify");
+    el.classList.add("notify--" + obj.type);
 
-        el.innerHTML = `
+    el.innerHTML = `
             <div class="notify__icon">
                 <i class="fa-solid ${icons[obj.type]}"></i>
             </div>
@@ -43,131 +43,145 @@ function notify(obj) {
             </div>
         `;
 
-        main.appendChild(el);
-        setTimeout(() => {
-            main.removeChild(main.firstElementChild);
-        }, 3500);
-    }
+    main.appendChild(el);
+    setTimeout(() => {
+      main.removeChild(main.firstElementChild);
+    }, 3500);
+  }
 }
 
 function handleSubmitSignup() {
-    console.log("Signup form submitted");
-    const username = document.querySelector("#signup__username").value;
-    const email = document.querySelector("#signup__email").value;
-    const password = document.querySelector("#signup__password").value;
-    if (!email || !password || !username) {
-        notify({
-            type: "warning",
-            msg: "Please fill all fields",
-        })
-        return;
-    }
+  console.log("Signup form submitted");
+  const username = document.querySelector("#signup__username").value;
+  const email = document.querySelector("#signup__email").value;
+  const password = document.querySelector("#signup__password").value;
+  if (!email || !password || !username) {
+    notify({
+      type: "warning",
+      msg: "Please fill all fields",
+    });
+    return;
+  }
 
-    // Send data to server
-    fetch("/user/signup", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, email, password }),
+  // Send data to server
+  fetch("/user/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
     })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            notify({
-                type: data.status,
-                msg: data.msg,
-            })
-            if (data.status === "success") {
-                setTimeout(() => {
-                    location.href = "/user/login";
-                }, 3500);
-            }
-        })
-        .catch((error) => {
-            notify({
-                type: "error",
-                msg: error.message,
-            })
-        });
+    .then((data) => {
+      notify({
+        type: data.status,
+        msg: data.msg,
+      });
+      if (data.status === "success") {
+        setTimeout(() => {
+          location.href = "/user/login";
+        }, 3500);
+      }
+    })
+    .catch((error) => {
+      notify({
+        type: "error",
+        msg: error.message,
+      });
+    });
 }
 
 function handleSubmitLogin() {
-    const useraccount = document.querySelector("#login__useraccount").value;
-    const password = document.querySelector("#login__password").value;
-    if (!useraccount || !password) {
-        notify({
-            type: "warning",
-            msg: "Please fill all fields",
-        });
-        return;
-    }
+  const useraccount = document.querySelector("#login__useraccount").value;
+  const password = document.querySelector("#login__password").value;
+  if (!useraccount || !password) {
+    notify({
+      type: "warning",
+      msg: "Please fill all fields",
+    });
+    return;
+  }
 
-    // Send data to server
-    fetch("/user/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ useraccount, password }),
+  // Send data to server
+  fetch("/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ useraccount, password }),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
     })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            notify({
-                type: data.status,
-                msg: data.msg,
-            });
+    .then((data) => {
+      notify({
+        type: data.status,
+        msg: data.msg,
+      });
 
-            if (data.status === "success") {
-                setTimeout(() => {
-                    location.href = "/";
-                }, 3500);
-            }
-        })
-        .catch((error) => {
-            alert("There was an error processing your request.");
-        });
+      if (data.status === "success") {
+        setTimeout(() => {
+          location.href = "/";
+        }, 3500);
+      }
+    })
+    .catch((error) => {
+      alert("There was an error processing your request.");
+    });
 }
 
 function handleLogout() {
-    console.log("Logout form submitted");
-    fetch("/user/logout", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
+  console.log("Logout form submitted");
+  fetch("/user/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
     })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            notify({
-                type: data.status,
-                msg: data.msg,
-            });
+    .then((data) => {
+      notify({
+        type: data.status,
+        msg: data.msg,
+      });
 
-            if (data.status === "success") {
-                setTimeout(() => {
-                    location.reload(true);
-                }, 3500);
-            }
-        })
-        .catch((error) => {
-            notify({
-                type: "error",
-                msg: error.message,
-            })
-        });
+      if (data.status === "success") {
+        setTimeout(() => {
+          location.reload(true);
+        }, 3500);
+      }
+    })
+    .catch((error) => {
+      notify({
+        type: "error",
+        msg: error.message,
+      });
+    });
 }
+
+// Discount
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+  });
+});
