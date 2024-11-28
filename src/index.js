06b44953737c0fc8f0241b9764c361654c03e9fe
VyBui13 <vybuoi0904@gmail.com>
2024-11-28 14:59:18 +0700

    Change language, delete banner in product details

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ app.engine("handlebars", engine({
   helpers: {
     eqString: (a, b) => String(a) === String(b),
     includes: (item, array) => Array.isArray(array) && array.includes(item),
+    formatPrice: (price) => {
+      return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    },
   },
 }));
 app.set("view engine", "handlebars");
