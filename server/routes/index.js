78031cdaf9038c9b3b26e2cd3453eab36a7ac7f8
ducnhat24 <ducnhaat@gmail.com>
2024-11-30 16:17:27 +0700

    22120252 - Add protected route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const userRoute = require('./user');
 const productRoute = require('./product');
+const { verifyToken } = require('../middleware/JWTAction');
 
 function route(app) {
     app.get('/', (req, res) => {
@@ -7,6 +8,10 @@ function route(app) {
     });
     app.use('/user', userRoute);
     app.use('/product', productRoute);
+    // Secret route
+    app.get('/protectiveroute', verifyToken, (req, res) => {
+        res.json({ message: `Success` });
+    });
 }
 
 module.exports = { route };
