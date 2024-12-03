eaee7cbde8571b45ef4dbe2e72e755a6574f8fa8
Linh_ASUS <vanlinh042004@gmail.com>
2024-12-03 09:25:03 +0700

    Linh - cart

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,19 @@
-const userRoute = require('./user');
-const productRoute = require('./product');
-const { verifyToken } = require('../middleware/JWTAction');
+const userRoute = require("./user");
+const productRoute = require("./product");
+const { verifyToken } = require("../middleware/JWTAction");
 
 function route(app) {
-    app.get('/', (req, res) => {
-        res.send('Hello from Express!');
-    });
-    app.use('/user', userRoute);
-    app.use('/product', productRoute);
-    // Secret route
-    app.get('/protectiveroute', verifyToken, (req, res) => {
-        res.json({ message: `Success` });
-    });
+  app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+  });
+  app.use("/user", userRoute);
+  app.use("/product", productRoute);
+  app.use("/cart", cartRoute);
+
+  // Secret route
+  app.get("/protectiveroute", verifyToken, (req, res) => {
+    res.json({ message: `Success` });
+  });
 }
 
 module.exports = { route };
--- server/schemas/User.js
+++ server/schemas/User.js
@@ -1,36 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    set: function () {
+      return Date.now();
+    },
+  },
+  refreshToken: {
+    type: String,
+    default: null,
+  },
+  cart: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId, // Tham chiếu đến `Product`
+        ref: "Product", // Model tham chiếu
         required: true,
-        unique: true,
-    },
-    name: {
-        type: String,
-        required: false,
-    },
-    password: {
-        type: String,
+      },
+      quantity: {
+        type: Number,
         required: true,
+        default: 0, // Số lượng mặc định
+      },
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-        set: function () {
-            return Date.now();
-        },
-    },
-    refreshToken: {
-        type: String,
-        default: null,
-    },
+  ],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
